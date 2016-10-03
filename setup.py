bae1f5d150290ef8d7513e5aaec33218eb536b86
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-03 12:53:20 +1100

    argh

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='yourscript',
     version='0.1',
-    py_modules=['yourscript'],
+    py_modules=['FactoryFactotum'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        yourscript=yourscript:cli
+        yourscript=FactoryFactotum:cli
     ''',
 )
